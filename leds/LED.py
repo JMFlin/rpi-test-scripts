43ec2f54c4a8f1cf7b66cca487dd7a094af8cad1
Janne Flinck <janne.flinck@yahoo.com>
2018-11-25 14:02:11 +0200

    bug fuxes

--- leds/LED.py
+++ leds/LED.py
@@ -3,20 +3,20 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-
-GPIO.setup(led_pins, GPIO.OUT)
-GPIO.output(led_pins, 1)
-
 led_pins = [2, 3, 4, 17]
+
+#GPIO.setup(led_pins, GPIO.OUT)
+#GPIO.output(led_pins, 1)]
 
 
 try:
     for i in led_pins:
-        GPIO.setup(led_pins, GPIO.OUT)
-        GPIO.output(led_pins, GPIO.HIGH)
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, GPIO.HIGH)
         print("Led", i, "is on")
         sleep(3)
         GPIO.output(i, GPIO.LOW)
+        sleep(3)
 except KeyboardInterrupt:
     pass
 
